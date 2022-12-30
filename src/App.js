4a049883ad126b9aa91d1eedc6b27e7e7f20c6b0
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-29 22:14:08 -0800

    Setup spotify authentication link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+    const CLIENT_ID = '0d1a27f4be9b4e709e5a030e624a5728'
+    const REDIRECT_URI = 'http://localhost:3000'
+    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+    const RESPONSE_TYPE = 'token'
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+          <header className="App-header">
+              <h1>Spotify React</h1>
+              <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+                  Login to Spotify
+              </a>
+          </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
